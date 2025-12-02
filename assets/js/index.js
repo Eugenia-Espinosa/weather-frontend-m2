5801c63edf211b53082a57b0bbc6172a10bd9cc0
Eugenia <kena.espinosa.r@gmail.com>
2025-12-02 16:50:33 -0300

    Barra de navegación

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,26 @@
 $(function () {
   let ciudades = [
-    { capital: "Rio de Janeiro",img: "rio_de_janeiro.jpg",temperatura: "10 C°",estado : "Parcial-nublado", icono: "🌥️", lunes : "12 C°🌥️", martes : "15 C° ",miercoles : "26 C°", jueves: "26 C°",viernes : "26 C°", sabado : "26 C°",domingo : "26 C°" },
-    { capital: "Buenos Aires",img: "buenos_aires.jpg", temperatura: "25 C°",estado : "nublado",icono: "🌞",lunes : "26 C°", martes : "26 C°",miercoles : "26 C°", jueves: "26 C°",viernes : "26 C°", sabado : "26 C°",domingo : "26 C°"},
-    { capital: "Santiago", img: "santiago.jpg", temperatura: "25 C°",estado : "nublado", icono:"☔",lunes : "26 C°", martes : "26 C°",miercoles : "26 C°", jueves: "26 C°",viernes : "26 C°", sabado : "26 C°",domingo : "26 C°" },
-    { capital: "Lima", img: "lima.jpg",temperatura: "25 C°",estado : "nublado",icono: "☔",lunes : "26 C°", martes : "26 C°",miercoles : "26 C°", jueves: "26 C°",viernes : "26 C°", sabado : "26 C°",domingo : "26 C°" },
-    { capital: "La Paz",img: "lapaz.jpg", temperatura: "25 C°",estado : "nublado",icono: "☔",lunes : "26 C°", martes : "26 C°",miercoles : "26 C°", jueves: "26 C°",viernes : "26 C°", sabado : "26 C°",domingo : "26 C°" },
-    { capital: "Montevideo",img: "montevideo.jpg",temperatura: "25 C°",estado : "nublado",icono: "☔",lunes : "26 C°", martes : "26 C°",miercoles : "26 C°", jueves: "26 C°",viernes : "26 C°", sabado : "26 C°",domingo : "26 C°" },
-    { capital: "Asunción",img: "asuncion.jpg",temperatura: "25 C°",estado : "nublado",icono: "☔",lunes : "26 C°", martes : "26 C°",miercoles : "26 C°", jueves: "26 C°",viernes : "26 C°", sabado : "26 C°",domingo : "26 C°" },
-    { capital: "Quito",img: "quito.jpg",temperatura: "25 C°",estado : "nublado",icono: "☔",lunes : "26 C°", martes : "26 C°",miercoles : "26 C°", jueves: "26 C°",viernes : "26 C°", sabado : "26 C°",domingo : "26 C°" },
-    { capital: "Caracas",img: "caracas.jpg",temperatura: "25 C°",estado : "nublado",icono: "☔",lunes : "26 C°", martes : "26 C°",miercoles : "26 C°", jueves: "26 C°",viernes : "26 C°", sabado : "26 C°",domingo : "26 C°" ,lunes : "26 C°", martes : "26 C°",miercoles : "26 C°", jueves: "26 C°",viernes : "26 C°", sabado : "26 C°",domingo : "26 C°" },
-    { capital: "Bogotá",img: "bogota.jpg",temperatura: "25 C°",estado : "nublado",icono: "☔",lunes : "26 C°", martes : "26 C°",miercoles : "26 C°", jueves: "26 C°",viernes : "26 C°", sabado : "26 C°",domingo : "26 C°" },
+    { capital: "Rio de Janeiro", img: "rio_de_janeiro.jpg", temperatura: "25 C°", estado: "Parcialmente nublado", icono: "🌥️", lunes: "Parcialmente nublado 24 C°🌥️", martes: "Parcialmente Nublado 23 C° 🌥️", miercoles: "Soleado 29 C° 🌞", jueves: "Soleado 26 C°🌞", viernes: "Parcialmente soleado 25 C° 🌤️", sabado: "Parcialmente Soleado 24 C° 🌤️", domingo: "Parcialmente Soleado 26 C°🌤️" },
+
+    { capital: "Buenos Aires", img: "buenos_aires.jpg", temperatura: "29 C°", estado: "Soleaso", icono: "🌞", lunes: "Parcialmente soleado 25 C° 🌤️", martes: "Parcialmente nublado 17 C°🌥️", miercoles: "Parcialmente nublado 15 C°🌥️", jueves: "Chubascos 17 C° 🌦️", viernes: "Lluvias 15 C° ☔", sabado: "Lluvias 15 C° ☔", domingo: "Parcialmente nublado 17 C°🌥️" },
+
+    { capital: "Santiago", img: "santiago.jpg", temperatura: "20 C°", estado: "Nublado", icono: "☁️", lunes: "Parcialmente nublado 12 C°🌥️", martes: "Parcialmente Nublado 15 C° 🌥️", miercoles: "Soleado 20 C° 🌞", jueves: "Soleado 26 C°🌞", viernes: "Parcialmente soleado 25 C° 🌤️", sabado: "Parcialmente Soleado 24 C° 🌤️", domingo: "Parcialmente Soleado 26 C°🌤️" },
+
+    { capital: "Lima", img: "lima.jpg", temperatura: "30 C°", estado: "Soleado", icono: "🌞", lunes: "Parcialmente soleado 25 C° 🌤️", martes: "Parcialmente nublado 17 C°🌥️", miercoles: "Parcialmente nublado 15 C°🌥️", jueves: "Chubascos 17 C° 🌦️", viernes: "Lluvias 15 C° ☔", sabado: "Lluvias 15 C° ☔", domingo: "Parcialmente nublado 17 C°🌥️" },
+
+    { capital: "La Paz", img: "lapaz.jpg", temperatura: "25 C°", estado: "nublado", icono: " ", lunes: "26 C°", martes: "26 C°", miercoles: "26 C°", jueves: "26 C°", viernes: "26 C°", sabado: "26 C°", domingo: "26 C°" },
+
+    { capital: "Montevideo", img: "montevideo.jpg", temperatura: "18 C°", estado: "Parcialmente nublado", icono: "🌥️", lunes: "Parcialmente nublado 12 C°🌥️", martes: "Parcialmente Nublado 15 C° 🌥️", miercoles: "Soleado 20 C° 🌞", jueves: "Soleado 26 C°🌞", viernes: "Parcialmente soleado 25 C° 🌤️", sabado: "Parcialmente Soleado 24 C° 🌤️", domingo: "Parcialmente Soleado 26 C°🌤️" },
+
+    { capital: "Asunción", img: "asuncion.jpg", temperatura: "29 C°", estado: "Soleaso", icono: "🌞", lunes: "Parcialmente soleado 25 C° 🌤️", martes: "Parcialmente nublado 17 C°🌥️", miercoles: "Parcialmente nublado 15 C°🌥️", jueves: "Chubascos 17 C° 🌦️", viernes: "Lluvias 15 C° ☔", sabado: "Lluvias 15 C° ☔", domingo: "Parcialmente nublado 17 C°🌥️" },
+
+    { capital: "Quito", img: "quito.jpg", temperatura: "25 C°", estado: "Parcialmente nublado", icono: "🌥️", lunes: "Parcialmente nublado 12 C°🌥️", martes: "Parcialmente Nublado 15 C° 🌥️", miercoles: "Soleado 20 C° 🌞", jueves: "Soleado 26 C°🌞", viernes: "Parcialmente soleado 25 C° 🌤️", sabado: "Parcialmente Soleado 24 C° 🌤️", domingo: "Parcialmente Soleado 26 C°🌤️" },
+
+    { capital: "Caracas", img: "caracas.jpg", temperatura: "25 C°", estado: "Parcialmente nublado", icono: "🌥️", lunes: "Parcialmente nublado 12 C°🌥️", martes: "Parcialmente Nublado 15 C° 🌥️", miercoles: "Soleado 20 C° 🌞", jueves: "Soleado 26 C°🌞", viernes: "Parcialmente soleado 25 C° 🌤️", sabado: "Parcialmente Soleado 24 C° 🌤️", domingo: "Parcialmente Soleado 26 C°🌤️" },
+
+    { capital: "Bogotá", img: "bogota.jpg", temperatura: "25 C°", estado: "Parcialmente nublado", icono: "🌥️", lunes: "Parcialmente nublado 12 C°🌥️", martes: "Parcialmente Nublado 15 C° 🌥️", miercoles: "Soleado 20 C° 🌞", jueves: "Soleado 26 C°🌞", viernes: "Parcialmente soleado 25 C° 🌤️", sabado: "Parcialmente Soleado 24 C° 🌤️", domingo: "Parcialmente Soleado 26 C°🌤️" },
   ]
-    
+
   let contenedor = $("#clima_container");
   console.log(contenedor);
   let tarjetas = "";
@@ -24,11 +33,12 @@ $(function () {
               <div class="card-body">
                 <h5 class="card-title">${value.capital}</h5>
                 <p class="card-text" >
-                    ${value.temperatura}
+                    ${value.estado}
                 </p>
                 <p class="card-text" >
-                    ${value.icono}
+                    ${value.temperatura} ${value.icono}
                 </p>
+                               
                 <a href="" class="btn btn-primary"       
                 data-bs-toggle="modal"
                 id="buttonModal-${index}"
@@ -49,12 +59,12 @@ $(function () {
           </div>
         `
   })
+
   contenedor.html(tarjetas)
 
   $(".btn-primary").click(function () {
     let temperatura = $(this).data("temperatura")
     let imagen = $(this).data("image")
-    let icono1 = $(this).data("iconos")
     let temp_lu = $(this).data("lun")
     let temp_ma = $(this).data("mar")
     let temp_mi = $(this).data("mie")
@@ -65,20 +75,17 @@ $(function () {
 
     $(".modal-body").html(
       `
-           <img src="./assets/img/${imagen}"  class="img-fluid" alt="" 
-
-            <p>${icono1}</p>
-            <p>Lunes ${temp_lu}</p>
-            <p>Martes ${temp_ma}</p>
-            <p>Miércoles ${temp_mi}</p>
-            <p>Jueves ${temp_ju}</p>
-            <p>Viernes ${temp_vi}</p>
-            <p>Sábado ${temp_sa}</p>
-            <pDomingo ${temp_do}</p>
-            
-
-
-           `
+      <img src="./assets/img/${imagen}"  class="img-fluid" alt="" 
+      <br>
+      <p>Lunes     :${temp_lu}</p>
+      <p>Martes    :${temp_ma}</p>
+      <p>Miércoles :${temp_mi}</p>
+      <p>Jueves    :${temp_ju}</p>
+      <p>Viernes   :${temp_vi}</p>
+      <p>Sábado    :${temp_sa}</p>
+      <p>Domingo   :${temp_do}</p>
+      
+       `
     )
   })
 })
